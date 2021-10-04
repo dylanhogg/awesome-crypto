b1fd7ed71f730b1c0622029e68d965192a16de86
dylan <dylanhogg@gmail.com>
2021-10-04 17:27:22 +1100

    Add org.html view over organisation aggregated repo data.

--- client/app/app.js
+++ client/app/app.js
@@ -9,8 +9,72 @@ function dateDiffInWeeks(a, b) {
 }
 
 $(document).ready( function () {
-    $("#table").DataTable( {
-        // deferRender: false,
+    // Github orgainisation table
+    $("#org-table").DataTable( {
+        ajax: {
+            // url: '/github_data_org.json',  // Local testing
+            url: 'https://crazy-awesome-crypto-api.infocruncher.com/github_data_org.json',
+            dataSrc: 'data'
+        },
+        initComplete: function(settings, json) {
+            var cnt = this.api().data().length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            $('#count').text(cnt);
+        },
+        order: [[ 4, "desc" ]],
+        paging: false,
+        columns: [
+          { title: "Github<br />Organisation",
+            render: function(data, type, row, meta) {
+                return "<a href='https://github.com/" + row.org + "'>" + row.org + "</a>";
+            }
+          },
+          { data: "repo_count", title: "Project<br />Count" },
+          { title: "Top Projects by Stars",
+            render: function(data, type, row, meta) {
+                var repos = row.repopath_first5.split(",");
+                var repos_links = repos.map(repo =>
+                    "<a href='https://github.com/" + repo + "' target='_blank'>" +
+                    "<img src='img/github.png' class='github-img'></img></a>&nbsp;" +
+                    "<a href='https://github.com/" + repo + "'>" + repo.split("/")[1] + "</a>");
+                return repos_links.slice(0, 3).join("<br />");
+            }
+          },
+          //{ data: "stars_min", title: "Stars<br />(min)", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          //{ data: "stars_median", title: "Stars<br />(med)", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          { data: "stars_max", title: "Max<br />Stars", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          { data: "stars_sum", title: "Sum of<br />Stars", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          //{ data: "stars_per_week_min", title: "Stars/wk<br />(min)", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          //{ data: "stars_per_week_mean", title: "Stars/wk<br />(mean)", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          //{ data: "stars_per_week_max", title: "Stars/wk<br />(max)", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          { data: "stars_per_week_sum", title: "Sum of<br />Stars/week", className: "text-nowrap", render: $.fn.dataTable.render.number(',', '.', 0) },
+          //{ data: "age_weeks_median", title: "Age weeks (med)" },
+          { title: "Project Age<br />Histogram",
+            render: function(data, type, row, meta) {
+                return row.age_weeks_hist + " weeks";
+            }
+          },
+          //{ data: "stars_hist", title: "Stars<br />Hist", orderable: false },
+          { data: "created_at_min", title: "Earliest<br />Created<br />Project",
+            className: "text-nowrap",
+            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
+          },
+          { data: "created_at_max", title: "Latest<br />Created<br />Project",
+            className: "text-nowrap",
+            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
+          },
+          { data: "updated_at_min", title: "Earliest<br />Updated<br />Project",
+            className: "text-nowrap",
+            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
+          },
+          { data: "updated_at_max", title: "Latest<br />Updated<br />Project",
+            className: "text-nowrap",
+            render: function(data, type, row, meta) { return new Date(data).toISOString().split('T')[0]; }
+          },
+        ],
+    });
+
+    // Github repo table
+    $("#repo-table").DataTable( {
         // dom: 'iftrip',  // https://datatables.net/reference/option/dom
         ajax: {
             // url: '/github_data.json',  // Local testing
@@ -39,9 +103,23 @@ $(document).ready( function () {
                 }
             }
           },
-          { data: "category", title: "Category" },
+          //{ data: "category", title: "Category" },
+//          { data: "_organization", title: "Organisation",
+//            render: function(data, type, row, meta) {
+//                return "<a href='https://github.com/" + data + "'>" + data.toLowerCase() + "</a>";
+//            }
+//          },
+          { data: "githuburl", title: "Github Project",
+            render: function(data, type, row, meta) {
+                return "<a href='" + row.githuburl + "'>" + row._repopath.toLowerCase() + "</a>";
+            }
+          },
           { data: "_description", title: "Description",
-            render: function(data, type, row, meta) { return "<div class='text-wrap description-column'>" + data + "</div>"; }
+            render: function(data, type, row, meta) {
+                var desc = row._reponame + ": " + row._description;
+                //var desc = "<a href='" + row.githuburl + "'>" + row._reponame + "</a>: "+ row._description;
+                return "<div class='text-wrap description-column'>" + desc + "</div>";
+            }
           },
           { title: "Links",
             render: function(data, type, row, meta) {
@@ -90,7 +168,7 @@ $(document).ready( function () {
         ],
     });
 
-    $('#table').on('click', '.modal-ajax', function(e) {
+    $("#repo-table").on('click', '.modal-ajax', function(e) {
         var localurl = $(this).data('localurl') + $(this).data('ext');
         e.preventDefault();
 
